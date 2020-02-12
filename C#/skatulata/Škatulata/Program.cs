6b72a967fc707f111f0d4d0ad387d3f001077904
Jan Dolejš <jandolejs1999@gmail.com>
2020-02-12 10:13:19 +0100

    change to painting

--- C#/skatulata/Škatulata/Program.cs
+++ C#/skatulata/Škatulata/Program.cs
@@ -1,39 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Škatulata
 {
 
-	struct Bod
-	{
+    struct Bod
+    {
 
-		public int X { get; set; }
-		public int Y { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
-		public override string ToString()
-		{
-			return string.Format("Pozice: [{0}, {1}]", X, Y);
-		}
-	}
-
-
+        public override string ToString()
+        {
+            return string.Format("Pozice: [{0}, {1}]", X, Y);
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start:");
+            Bod pozice = new Bod();
 
-            Bod pozice_default = new Bod();
+            pozice.X = Console.WindowWidth/2;
+            pozice.Y = Console.WindowHeight/2;
 
-            pozice_default.X = Console.WindowWidth/2;
-            pozice_default.Y = Console.WindowHeight/2;
+            Stack<Bod> postup = new Stack<Bod>(); // vytvoří zásobník
+            postup.Push(pozice); // přidání prvku do zásobníku
 
-            Console.SetCursorPosition(pozice_default.X, pozice_default.Y);
-            Console.Write("X");
+            // Bod prvek_chci = postup.Pop();
 
-            Console.ReadKey();
+            while (true) {
+                // reakce na klávesy
+                ConsoleKeyInfo key_info = Console.ReadKey();
 
+                if (key_info.Key == ConsoleKey.UpArrow)
+                    pozice.Y--;
+                else if (key_info.Key == ConsoleKey.RightArrow)
+                    pozice.X++;
+                else if (key_info.Key == ConsoleKey.DownArrow)
+                    pozice.Y++;
+                else if (key_info.Key == ConsoleKey.LeftArrow)
+                    pozice.X--;
+
+                Console.SetCursorPosition(pozice.X, pozice.Y);
+                Console.Write("X");
+                Console.CursorLeft--;
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
