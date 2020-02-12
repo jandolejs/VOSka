5eeeae8b06730d11bb7d3b1af2e13e0f1f761c0d
Jan Dolejš <jandolejs1999@gmail.com>
2020-02-12 09:33:36 +0100

    Init Škatulata
    nějaká blbost s myší nebo co

---
+++ C#/skatulata/Škatulata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Škatulata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
