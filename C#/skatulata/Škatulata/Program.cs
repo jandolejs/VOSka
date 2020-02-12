2901407f4d3072b74ecf2b2e1451b700c487febd
Jan Dolejš <jandolejs1999@gmail.com>
2020-02-12 09:36:28 +0100

    add struct

--- C#/skatulata/Škatulata/Program.cs
+++ C#/skatulata/Škatulata/Program.cs
@@ -2,6 +2,21 @@
 
 namespace Škatulata
 {
+
+	struct Bod
+	{
+
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public override string ToString()
+		{
+			return X + " " + Y;
+		}
+	}
+
+
+
     class Program
     {
         static void Main(string[] args)
